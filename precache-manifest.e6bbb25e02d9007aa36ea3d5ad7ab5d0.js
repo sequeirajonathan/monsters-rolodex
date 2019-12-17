0491fb26290f8bb190fc1d5dc0f47f88846edc7e
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2019-12-17 14:40:52 -0500

    Updates

---
+++ precache-manifest.e6bbb25e02d9007aa36ea3d5ad7ab5d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e2c2891631545de696216e9c7b5e18",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1516b1824541fba3f072",
+    "url": "/monsters-rolodex/static/css/main.8c29d859.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1516b1824541fba3f072",
+    "url": "/monsters-rolodex/static/js/main.66d7f974.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
